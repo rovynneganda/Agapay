bdbc2ec9aaf25e507271b1816f6c91d162e655a9
cjhay06 <145081105+cjhay06@users.noreply.github.com>
2023-10-27 23:11:45 -0400

    regform

--- agapay/src/components/User/Login.jsx
+++ agapay/src/components/User/Login.jsx
@@ -128,8 +128,8 @@ const Login = ({ onClose }) => {
     setLoginModalVisible(false);
     setRegisterModalVisible(true);
   };
-  const handleClick = (e) => {
-    e.preventDefault();
+  useEffect(()=>{
+    
     const status = document.querySelector(".registrationAddress");
     const success = (position) => {
       // console.log(position)
@@ -141,6 +141,16 @@ const Login = ({ onClose }) => {
       fetch(geoApiUrl)
         .then((res) => res.json())
         .then((data) => {
+
+          // Get PostalCode 
+           const regPostalCode = data.results[0].address_components.find(
+            (component) => component.types.includes('postal_code')
+            );            
+            // end to Get PostalCode 
+            document.querySelector("#regPostalCode").value = regPostalCode.short_name;
+           console.log(regPostalCode);
+
+
           const address = data.results[0].formatted_address;
           const addressArray = address.split(', ');
           console.log(data.results[0].formatted_address);
@@ -148,6 +158,8 @@ const Login = ({ onClose }) => {
           for (let i = addressArray.length - 4; i >= 0; i--) {
             selectedStreetBrgy.push(addressArray[i]);
           }
+          console.log(selectedStreetBrgy)
+          console.log(addressArray)
           document.querySelector("#regStreetBrgy").value = selectedStreetBrgy.join(', ');
           document.querySelector("#regCity").value = addressArray[addressArray.length - 3];
           status.textContent = data.results[0].formatted_address;
@@ -158,7 +170,7 @@ const Login = ({ onClose }) => {
     };
 
     navigator.geolocation.getCurrentPosition(success, error);
-  };
+  },[])
   // console.log(document.querySelector(".registrationAddress"));
   return (
     <>
@@ -569,8 +581,7 @@ const Login = ({ onClose }) => {
                     </option>
                     <option value="US">United States</option>
                   </select> */}
-                  <button onClick={handleClick}>eme lang</button>
-                  <p className="registrationAddress"></p>
+                
                   <div className="relative">
                     <input
                       type="text"
