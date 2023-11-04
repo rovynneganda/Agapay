2c0408917db569b5c75911eea6d57a3cec4e35f3
Rovynne Reyes <reyesrobin01234@gmail.com>
2023-11-04 14:02:35 +0800

    changing layouts

--- agapay/src/weather/components/Forecast.jsx
+++ agapay/src/weather/components/Forecast.jsx
@@ -1,37 +1,26 @@
-import React from 'react'
-import { iconUrlFromCode } from '../services/weatherServices'
+import React from "react";
+import { iconUrlFromCode } from "../services/weatherServices";
 
-function Forecast({title,items}) {
+function Forecast({ title, items }) {
   return (
     <div>
-        <div className='flex items-center justify-start mt-3'>
-            <p className='text-black font-bold  uppercase font-inter leading-relaxed '>{title}</p>
-
-        </div>
-      <hr className='my-2 text-black'></hr> 
-       <div className='flex flex-row items-center justify-between text-black'>
-        {items.map(item => (
-
-<div className='flex flex-col items-center justify-center'>
-<p className='font-light text-sm'>
-   {item.title}
-</p>
-<img
- src={iconUrlFromCode(item.icon)}
- className='' 
- alt=""
-   />
-<p className='font-medium'>{`${item.temp.toFixed()}°`}</p>
-
-</div>
-
+      <div className="flex items-center justify-start mt-3">
+        <p className="text-black font-bold  uppercase font-inter leading-relaxed ">
+          {title}
+        </p>
+      </div>
+      <hr className="my-2 text-black"></hr>
+      <div className="flex flex-row items-center justify-between text-black">
+        {items.map((item) => (
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-light text-sm">{item.title}</p>
+            <img src={iconUrlFromCode(item.icon)} className="" alt="" />
+            <p className="font-medium">{`${item.temp.toFixed()}°`}</p>
+          </div>
         ))}
-       
-       
-       </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Forecast
+export default Forecast;
