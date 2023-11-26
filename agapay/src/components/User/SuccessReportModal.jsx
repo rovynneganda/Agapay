994032d9ba54268c8d140af7b503176a9b6a371b
Rovynne Reyes <reyesrobin01234@gmail.com>
2023-11-26 22:42:36 +0800

    added loader svg in the video and reporting, also added a success popup after the report

--- agapay/src/components/User/SuccessReportModal.jsx
+++ agapay/src/components/User/SuccessReportModal.jsx
@@ -1,32 +1,31 @@
 import React from "react";
 import { CheckCircleIcon, XMarkIcon } from "@heroicons/react/24/outline";
-const SuccessReportModal = () => {
+const SuccessReportModal = ({closeSuccessModal}) => {
   return (
     <>
       {/* For Success modal */}
       <div
-        tabindex="-1"
-        className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center p-4 overflow-x-hidden hidden bg-gray bg-opacity-50 overflow-y-auto md:inset-0 h-[calc(100%)] max-h-full"
+        tabIndex="-1"
+        className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center p-4 overflow-x-hidden  bg-black bg-opacity-50 overflow-y-auto md:inset-0 h-[calc(100%)] max-h-full"
       >
         <div className="relative w-full max-w-md max-h-full">
           <div className="relative bg-white rounded-lg shadow ">
-            <button
+            {/* <button
               type="button"
               className="absolute top-3 right-2.5 text-gray bg-transparent hover:bg-gray/20 hover:text-gray rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center "
             >
               <XMarkIcon className="w-5 h-5" />
               <span className="sr-only">Close modal</span>
-            </button>
+            </button> */}
             <div className="p-6 text-center">
-              <CheckCircleIcon className="h-12 w-12 mb-5 mx-auto text-secondary" />
-              <h3 className="mb-5 text-lg font-normal text-gray font-inter ">
-                The report has been successfully Submitted, please wait for the
-                approval of the Admin.
+              <CheckCircleIcon className="h-16 w-16 mb-5 mx-auto text-secondary" />
+              <h3 className="mb-5 text-xl font-normal text-gray font-inter ">
+                The report has been successfully submitted.
               </h3>
               <button
-                data-modal-hide="popup-modal"
                 type="button"
-                className="text-white bg-red hover:bg-red/80 focus:ring-4 focus:outline-none focus:ring-red-300  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+                onClick={closeSuccessModal}
+                className="text-white font-inter bg-red hover:bg-red/80 focus:ring-4 focus:outline-none focus:ring-red  font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
               >
                 Close
               </button>
