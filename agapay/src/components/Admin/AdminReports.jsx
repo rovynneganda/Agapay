5aa19aa4647d80816d9a64d170c8d1f6188cedf3
jepoy333 <149779021+jepoy333@users.noreply.github.com>
2023-12-02 01:54:13 +0800

    Update AdminReports.jsx

--- agapay/src/components/Admin/AdminReports.jsx
+++ agapay/src/components/Admin/AdminReports.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
-import { CheckBadgeIcon, ExclamationCircleIcon, ArrowPathRoundedSquareIcon } from "@heroicons/react/20/solid";
+import {
+  CheckBadgeIcon,
+  ExclamationCircleIcon,
+  ArrowPathRoundedSquareIcon,
+} from "@heroicons/react/20/solid";
 import { ArrowRightIcon, ShieldCheckIcon } from "@heroicons/react/24/outline";
 const AdminReports = () => {
   const [reportData, setReportData] = useState([]);
@@ -40,34 +44,35 @@ const AdminReports = () => {
       if (Object.hasOwnProperty.call(vehicleCountData, vehicleType)) {
         const count = vehicleCountData[vehicleType];
         if(count !== 0){
-          // Create a new FormData for each vehicle type
-          const formData = new FormData();
-          formData.append("fileSelector", "gis");
-          formData.append("department", vehicleType);
-          formData.append("vehicleCount", count);
-          formData.append("reportId", id);
-      
-          // Send Axios request for each vehicle type
-          axios
-            .post(
-              "http://localhost/Backend/Controller.php",
-              formData,
-              {
-                withCredentials: true,
-                headers: {
-                  "Content-Type": "multipart/form-data",
-                },
-              }
-            )
-            .then((response) => {
-              // Handle the response for each vehicle type as needed
-              console.log(response)
-              console.log(`Request for ${vehicleType} succeeded:`, response.data);
-            })
-            .catch((error) => {
-              console.error(`Error for ${vehicleType}:`, error);
-            });
+          
         }
+        // Create a new FormData for each vehicle type
+        const formData = new FormData();
+        formData.append("fileSelector", "gis");
+        formData.append("department", vehicleType);
+        formData.append("vehicleCount", count);
+        formData.append("reportId", id);
+    
+        // Send Axios request for each vehicle type
+        axios
+          .post(
+            "http://localhost/Backend/Controller.php",
+            formData,
+            {
+              withCredentials: true,
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            }
+          )
+          .then((response) => {
+            // Handle the response for each vehicle type as needed
+            console.log(response)
+            console.log(`Request for ${vehicleType} succeeded:`, response.data);
+          })
+          .catch((error) => {
+            console.error(`Error for ${vehicleType}:`, error);
+          });
       }
     }
   };
@@ -113,58 +118,89 @@ const AdminReports = () => {
         <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
           <div className="grid sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-3 gap-4 sm:gap-6">
             {reportData.map((report, index) => (
-              <div key={index} className=" flex flex-col bg-white border border-gray/20  rounded-xl hover:shadow-md transition shadow-lg ">
+              <div
+                key={index}
+                className=" flex flex-col bg-white border border-gray/20  rounded-xl hover:shadow-md transition shadow-lg "
+              >
                 <div className="p-4 md:p-5">
                   <div className="flex justify-between items-center">
                     <div>
                       <div className="flex flex-row justify-between">
-                      <h3 className="text-primary font-bold  font-inter text-2xl">
-                        {report.disaster} 
-                      
-                      </h3>
-                    <div className="flex flex-col">
-                      <button disabled className={`${report.ai_verify === '1' ? '  ' : ''} rounded-full font-inter font-medium  text-sm`}>
-                        {report.ai_verify === '1' ? <>
-                        <CheckBadgeIcon className="h-6 w-6 text-secondary rounded-full inline-block "/> <span className="text-secondary font-poppins">Verified</span>
-                        </> : <>
-                        <ExclamationCircleIcon className="h-6 w-6 text-red rounded-full inline-block "/> <span className="text-red font-poppins">    Invalid Verification </span>
+                        <h3 className="text-primary font-bold  font-inter text-2xl">
+                          {report.disaster}
+                        </h3>
+                        <div className="flex flex-col">
+                          <button
+                            disabled
+                            className={`${
+                              report.ai_verify === "1" ? "  " : ""
+                            } rounded-full font-inter font-medium  text-sm`}
+                          >
+                            {report.ai_verify === "1" ? (
+                              <>
+                                <CheckBadgeIcon className="h-6 w-6 text-secondary rounded-full inline-block " />{" "}
+                                <span className="text-secondary font-poppins">
+                                  Verified
+                                </span>
+                              </>
+                            ) : (
+                              <>
+                                <ExclamationCircleIcon className="h-6 w-6 text-red rounded-full inline-block " />{" "}
+                                <span className="text-red font-poppins">
+                                  {" "}
+                                  Invalid Verification{" "}
+                                </span>
+                              </>
+                            )}
+                          </button>
+                          <button
+                            disabled
+                            className="rounded-full font-inter font-medium text-sm"
+                          >
+                            <ArrowPathRoundedSquareIcon className="w-6 h-6 text-secondary rounded-full inline-block " />{" "}
+                            <span className="font-poppins ">Pending</span>
+                          </button>
+                          <button
+                            disabled
+                            className="rounded-full font-inter font-medium text-sm"
+                          >
+                            <ArrowRightIcon className="w-6 h-6 text-secondary rounded-full inline-block " />{" "}
+                            <span className="font-poppins">On Going</span>
+                          </button>
+                          <button
+                            disabled
+                            className="rounded-full font-inter font-medium text-sm"
+                          >
+                            <ShieldCheckIcon className="w-6 h-6 text-secondary rounded-full inline-block " />{" "}
+                            <span className="font-poppins">Accomplished</span>
+                          </button>
+                        </div>
+                      </div>
 
-                    
-                        </>}
-                      </button>
-                    <button disabled className="rounded-full font-inter font-medium text-sm">
-                      <ArrowPathRoundedSquareIcon className="w-6 h-6 text-secondary rounded-full inline-block "/> <span className="font-poppins ">Pending</span>
-                    </button>
-                    <button disabled className="rounded-full font-inter font-medium text-sm">
-                      <ArrowRightIcon className="w-6 h-6 text-secondary rounded-full inline-block "/> <span className="font-poppins">On Going</span>
-                    </button>
-                    <button disabled className="rounded-full font-inter font-medium text-sm">
-                      <ShieldCheckIcon className="w-6 h-6 text-secondary rounded-full inline-block "/> <span className="font-poppins">Accomplished</span>
-                    </button>
-                      </div>
-                      </div>
-                    
                       <div className="space-y-1">
-                      <p className="text-lg font-poppins   font-semibold">
-                        {report.user_id} name
-                      </p>
-                      <p className="text-base  font-poppins  font-medium">
-                        {report.user_id} contact number
-                      </p>
-                      <p className="text-base  font-poppins  font-medium">
-                        {report.disaster_address}
-                      </p>
-                      <p className="text-base   font-poppins border border-dashed rounded-xl p-1 border-gray/20">
-                        {report.description}
-                      </p>
-                      <p className="text-base  font-poppins text-center mt-3  rounded-xl p-1 text-darkblue font-semibold hover:underline ">
-                        {report.video}
-                      </p>
+                        <p className="text-lg font-poppins   font-semibold">
+                          {report.user_id} name
+                        </p>
+                        <p className="text-base  font-poppins  font-medium">
+                          {report.user_id} contact number
+                        </p>
+                        <p className="text-base  font-poppins  font-medium">
+                          {report.disaster_address}
+                        </p>
+                        <p className="text-base   font-poppins border border-dashed rounded-xl p-1 border-gray/20">
+                          {report.description}
+                        </p>
+                        <p className="text-base  font-poppins text-center mt-3  rounded-xl p-1 text-darkblue font-semibold hover:underline ">
+                          {report.video}
+                        </p>
                       </div>
                       <div>
                         <video width="800" height="600" controls>
                           {/* Replace 'your_video_url' with the actual URL of your video */}
-                          <source src={`http://localhost/Backend/Videos/Upload/${report.video}`} type="video/mp4" />
+                          <source
+                            src={`http://localhost/Backend/Videos/Upload/${report.video}`}
+                            type="video/mp4"
+                          />
                           Your browser does not support the video tag.
                         </video>
                       </div>
@@ -197,7 +233,10 @@ const AdminReports = () => {
                         </div>
                       )}
                       <div className="flex flex-row gap-3 justify-center mt-2">
-                        <button className="px-2 py-2 bg-primary rounded-xl font-inter font-medium text-white text-sm hover:bg-primarydark">
+                        <button
+                          onClick={() => sendReport(report.id)}
+                          className="px-2 py-2 bg-primary rounded-xl font-inter font-medium text-white text-sm hover:bg-primarydark"
+                        >
                           Send Responder
                         </button>
                         <button className="text-[#c94c05] hover:text-white border border-red font-inter bg-none hover:bg-red focus:outline-none   font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
